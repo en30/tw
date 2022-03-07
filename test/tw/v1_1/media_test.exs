516e7959a79737f480fa77469b8fdcce166bc95e
en30 <en30.git@gmail.com>
2022-03-07 20:03:11 +0900

    Add tests for Tw.V1_1.Media.upload/2

--- test/tw/v1_1/media_test.exs
+++ test/tw/v1_1/media_test.exs
@@ -112,5 +112,241 @@ defmodule Tw.V1_1.MediaTest do
 
       assert {:ok, %{media_id: @media_id}} = Media.upload(client, %{data: @bin, media_type: "image/png"})
     end
+
+    test "splits a large file into chunks" do
+      size = 1024 * 1024 + 100
+      data = for _ <- 1..size, into: "", do: <<0>>
+
+      client =
+        stub_client([
+          {
+            {:post, "https://upload.twitter.com/1.1/media/upload.json",
+             ~r"""
+             --.*?\r
+             Content-Disposition: form-data; name="command"\r
+             \r
+             INIT\r
+             --.*?\r
+             Content-Disposition: form-data; name="media_category"\r
+             \r
+             tweet_video\r
+             --.*?\r
+             Content-Disposition: form-data; name="media_type"\r
+             \r
+             video/mp4\r
+             --.*?\r
+             Content-Disposition: form-data; name="total_bytes"\r
+             \r
+             #{size}\r
+             --.*?--\r
+             """m},
+            json_response(200, """
+            {
+              "media_id": #{@media_id},
+              "media_id_string": "#{@media_id}",
+              "size": 68,
+              "expires_after_secs": 86400,
+              "image": {
+                "image_type": "image/png",
+                "w": 1,
+                "h": 1
+              }
+            }
+            """)
+          },
+          {
+            {:post, "https://upload.twitter.com/1.1/media/upload.json",
+             ~r"""
+             --.*?\r
+             Content-Disposition: form-data; name="command"\r
+             \r
+             APPEND\r
+             --.*?\r
+             Content-Disposition: form-data; name="media"\r
+             \r
+             #{for _ <- 1..(1024 * 1024), into: "", do: <<0>>}\r
+             --.*?\r
+             Content-Disposition: form-data; name="media_id"\r
+             \r
+             #{@media_id}\r
+             --.*?\r
+             Content-Disposition: form-data; name="segment_index"\r
+             \r
+             0\r
+             --.*?--\r
+             """m},
+            html_response(200, "")
+          },
+          {
+            {:post, "https://upload.twitter.com/1.1/media/upload.json",
+             ~r"""
+             --.*?\r
+             Content-Disposition: form-data; name="command"\r
+             \r
+             APPEND\r
+             --.*?\r
+             Content-Disposition: form-data; name="media"\r
+             \r
+             #{for _ <- 1..100, into: "", do: <<0>>}\r
+             --.*?\r
+             Content-Disposition: form-data; name="media_id"\r
+             \r
+             #{@media_id}\r
+             --.*?\r
+             Content-Disposition: form-data; name="segment_index"\r
+             \r
+             1\r
+             --.*?--\r
+             """m},
+            html_response(200, "")
+          },
+          {
+            {:post, "https://upload.twitter.com/1.1/media/upload.json",
+             ~r"""
+             --.*?\r
+             Content-Disposition: form-data; name="command"\r
+             \r
+             FINALIZE\r
+             --.*?\r
+             Content-Disposition: form-data; name="media_id"\r
+             \r
+             #{@media_id}\r
+             --.*?--\r
+             """m},
+            json_response(200, """
+            {
+              "media_id": #{@media_id},
+              "media_id_string": "#{@media_id}",
+              "size": 68,
+              "expires_after_secs": 86400
+            }
+            """)
+          }
+        ])
+
+      assert {:ok, %{media_id: @media_id}} = Media.upload(client, %{data: data, media_type: "video/mp4"})
+    end
+
+    test "waits for processing if necessary" do
+      client =
+        stub_client([
+          {
+            {:post, "https://upload.twitter.com/1.1/media/upload.json",
+             ~r"""
+             --.*?\r
+             Content-Disposition: form-data; name="command"\r
+             \r
+             INIT\r
+             --.*?\r
+             Content-Disposition: form-data; name="media_category"\r
+             \r
+             tweet_image\r
+             --.*?\r
+             Content-Disposition: form-data; name="media_type"\r
+             \r
+             image/png\r
+             --.*?\r
+             Content-Disposition: form-data; name="total_bytes"\r
+             \r
+             68\r
+             --.*?--\r
+             """m},
+            json_response(200, """
+            {
+              "media_id": #{@media_id},
+              "media_id_string": "#{@media_id}",
+              "size": 68,
+              "expires_after_secs": 86400,
+              "image": {
+                "image_type": "image/png",
+                "w": 1,
+                "h": 1
+              }
+            }
+            """)
+          },
+          {
+            {:post, "https://upload.twitter.com/1.1/media/upload.json",
+             ~r"""
+             --.*?\r
+             Content-Disposition: form-data; name="command"\r
+             \r
+             APPEND\r
+             --.*?\r
+             Content-Disposition: form-data; name="media"\r
+             \r
+             #{@bin}\r
+             --.*?\r
+             Content-Disposition: form-data; name="media_id"\r
+             \r
+             #{@media_id}\r
+             --.*?\r
+             Content-Disposition: form-data; name="segment_index"\r
+             \r
+             0\r
+             --.*?--\r
+             """m},
+            html_response(200, "")
+          },
+          {
+            {:post, "https://upload.twitter.com/1.1/media/upload.json",
+             ~r"""
+             --.*?\r
+             Content-Disposition: form-data; name="command"\r
+             \r
+             FINALIZE\r
+             --.*?\r
+             Content-Disposition: form-data; name="media_id"\r
+             \r
+             #{@media_id}\r
+             --.*?--\r
+             """m},
+            json_response(200, """
+            {
+              "media_id": #{@media_id},
+              "media_id_string": "#{@media_id}",
+              "size": 68,
+              "expires_after_secs": 86400,
+              "processing_info": {
+                "state": "pending",
+                "check_after_secs": 0
+              }
+            }
+            """)
+          },
+          {
+            {:get, "https://upload.twitter.com/1.1/media/upload.json?command=STATUS&media_id=#{@media_id}"},
+            json_response(200, """
+            {
+              "media_id": #{@media_id},
+              "media_id_string": "#{@media_id}",
+              "size": 68,
+              "expires_after_secs": 86400,
+              "processing_info": {
+                "state": "in_progress",
+                "check_after_secs": 0
+              }
+            }
+            """)
+          },
+          {
+            {:get, "https://upload.twitter.com/1.1/media/upload.json?command=STATUS&media_id=#{@media_id}"},
+            json_response(200, """
+            {
+              "media_id": #{@media_id},
+              "media_id_string": "#{@media_id}",
+              "size": 68,
+              "expires_after_secs": 86400,
+              "processing_info": {
+                "state": "succeeded",
+                "progress_percent": 100
+              }
+            }
+            """)
+          }
+        ])
+
+      assert {:ok, %{media_id: @media_id}} = Media.upload(client, %{data: @bin, media_type: "image/png"})
+    end
   end
 end
